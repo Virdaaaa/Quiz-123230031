04b1e7af13310e7acda8de7242a740e9c6584dd9
Virda <firdaalifatuzzahroo0031@gmail.com>
2026-03-11 22:48:02 +0700

    revisi

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:belajar_kuisTPM/screen/login_page.dart'; // ini juga harus sesuai sama sama folder yang ada di file manager
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const MinilenstApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MinilenstApp extends StatelessWidget {
+  const MinilenstApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
+      home: const LoginPage(),
     );
   }
 }
--- lib/screen/login_page.dart
+++ lib/screen/login_page.dart
@@ -1,19 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:belajar_kuisTPM/screen/movie_list_page.dart'; // habis dari login langsung ke movie list page, jadi harus import movie list page juga yaa
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _userController = TextEditingController();
+  final _passController = TextEditingController();
+
+  void _login() {
+    // Mengecek apakah username adalah "virda" DAN password adalah "031"
+    if (_userController.text == "virda" && _passController.text == "031") {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) =>
+              MovieListPage(username: (_userController.text).toString()),
+        ),
+      );
+    } else {
+      // Jika salah satu atau keduanya salah
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Username atau Password salah!")),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    /*
-      buatlah tampilan login sederhana dengan menggunakan widget TextField untuk input username dan password, serta ElevatedButton untuk tombol login.
-      - TextField untuk username dengan hint text "Username"
-      - TextField untuk password dengan hint text "Password" dan obscureText true
-      - ElevatedButton dengan text "Login" yang ketika ditekan akan menampilkan snackbar dengan pesan
-        "Login berhasil" jika username dan password tidak kosong, atau "Login gagal" jika salah satu atau kedua field kosong.
-      - jika login berhasil, navigasikan ke halaman MovieListPage (buat halaman MovieListPage 
-    
-     */
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Login",
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+            const Text("Welcome back to Minilenst"),
+            const SizedBox(height: 30),
+            TextField(
+              controller: _userController,
+              decoration: const InputDecoration(labelText: "Username"),
+            ),
+            TextField(
+              controller: _passController,
+              decoration: const InputDecoration(labelText: "Password"),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: _login,
+                child: const Text("Login"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
